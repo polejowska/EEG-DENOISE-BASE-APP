31d89941666d13a345aed0840aa35ae2f91853ab
Agata Polejowska <polejowska.agata@gmail.com>
2022-04-02 10:24:19 +0200

    Add the basic template

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def read_root(request: Request):
+     return templates.TemplateResponse("start.html", {"request": request})
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("items.html", {"request": request, "id": id})
