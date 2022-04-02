f0f673b420ad2b859d44f6598e99e82f2c454c4c
Agata Polejowska <polejowska.agata@gmail.com>
2022-04-02 14:49:02 +0200

    Add working examples of eeg generator

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from enum import Enum
 
+from EEGGenerator import EEGGenerator
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -15,13 +17,15 @@ templates = Jinja2Templates(directory="templates")
 class AlgorithmName(str, Enum):
     ica = "ICA"
     dwt = "DWT"
-    ar ="AR"
+    ar = "AR"
 
 
 @app.get('/eeg-generator')
 async def eeg_generator(request: Request):
+    eeg_generator = EEGGenerator(fs=256, start_time=0, end_time=10)
+    signal = eeg_generator.simulate_eeg()
+    eeg_generator.visualize(signal)
     return templates.TemplateResponse("synthetic-eeg-generator.html", {"request": request})
-
 
 @app.get('/eeg-loader')
 async def eeg_loader(request: Request):
