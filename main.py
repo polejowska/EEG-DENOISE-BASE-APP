48218c52c620bb6eccaa6dfac4b7d1d90a1611d9
Agata Polejowska <polejowska.agata@gmail.com>
2022-04-02 13:08:34 +0200

    Update project - add essential structures

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from enum import Enum
 
 app = FastAPI()
 
@@ -10,10 +12,30 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
+class AlgorithmName(str, Enum):
+    ica = "ICA"
+    dwt = "DWT"
+    ar ="AR"
+
+
+@app.get('/eeg-generator')
+async def eeg_generator(request: Request):
+    return templates.TemplateResponse("synthetic-eeg-generator.html", {"request": request})
+
+
+@app.get('/eeg-loader')
+async def eeg_loader(request: Request):
+    return templates.TemplateResponse("eeg-loader.html", {"request": request})
+
+
 @app.get("/")
 async def read_root(request: Request):
      return templates.TemplateResponse("start.html", {"request": request})
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("items.html", {"request": request, "id": id})
+
+@app.get("/algorithm/{algorithm_name}")
+async def get_algorithm(algorithm_name: AlgorithmName):
+    if algorithm_name == AlgorithmName.ica:
+        return {"algorithm_name": algorithm_name, "message": "ICA"}
+
+    return {"algorithm_name": "None", "message": "Choose algorithm"}
